55396ea4259c315c4ac5ad03b3cdbf076034f3e4
Sophie Mullen <sophie.mullen@cegeka.com>
2017-02-23 16:57:58 +0100

    thursday

---
+++ Order/src/main/java/be/cegeka/orders/order/domain/Stock/Stock.java
@@ -0,0 +1,42 @@
+package be.cegeka.orders.order.domain.Stock;
+
+import be.cegeka.orders.order.domain.items.Item;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name = "Stock")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STOCK_ID")
+    private int id;
+
+    @OneToOne(cascade = ALL, mappedBy = "ITEM_ID")
+//    @JoinTable(
+//            name = "Stock",
+//            joinColumns = {@JoinColumn(name = "STOCK_ID")},
+//            inverseJoinColumns = {@JoinColumn(name = "ITEM_ID")})
+    private Item item;
+
+    public Stock() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addItem(Item item) {
+        this.item = item;
+    }
+
+//    public List<Item> getItems() {
+//        return items;
+//    }
+}
---
+++ Order/src/test/java/be/cegeka/orders/order/domain/Stock/StockRepositoryTest.java
@@ -0,0 +1,53 @@
+package be.cegeka.orders.order.domain.Stock;
+
+import be.cegeka.orders.order.OrderApplication;
+import be.cegeka.orders.order.domain.items.Item;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = OrderApplication.class)
+@Transactional
+public class StockRepositoryTest {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Inject
+    private StockRepository stockRepository;
+    private List<Stock> stockItems;
+    private Item item;
+    private Stock stock;
+
+
+    @Before
+    public void setUpDatabase(){
+        stockItems = new ArrayList<>();
+       // item = new Item("smurf", "blue", 2.0);
+
+        entityManager.persist(stockItems);
+      //  entityManager.persist(item);
+    }
+
+    @Test
+    public void getAllStock_returnsAllStock() throws Exception{
+        assertThat(stockRepository.getAllStock()).contains(stock);
+    }
+
+}
