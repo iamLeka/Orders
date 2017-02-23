0a502bfe8af54acb1c9b130457c39dc1e1b63c24
Kevin Smet <kevin.smet@cegeka.com>
2017-02-23 16:45:19 +0100

    updated orders

--- Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
@@ -1,10 +1,49 @@
 package be.cegeka.orders.order.domain.orders;
 
-/**
- * Created by stijnli on 23/02/2017.
- */
+import be.cegeka.orders.order.domain.customers.Customer;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "Orders")
 public class Order {
 
+    @Id
+    //@GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "ORDER_ID")
+    private int id;
+    @Column (name = "ORDER_DATE")
+    private Date orderDate;
 
+   public Order(){
+
+    }
+
+    public Order ( Date orderDate){
+        this.orderDate = orderDate;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return id == order.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    public int getOrderId() {
+        return id;
+
+    }
 
 }
--- Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderRepositoryTest.java
+++ Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderRepositoryTest.java
@@ -1,22 +1,28 @@
 package be.cegeka.orders.order.domain.orders;
 
 import be.cegeka.orders.order.OrderApplication;
+import be.cegeka.orders.order.domain.customers.Customer;
+import be.cegeka.orders.order.domain.customers.CustomerRepository;
+import org.apache.poi.sl.draw.geom.CosExpression;
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.util.calendar.BaseCalendar;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
-/**
- * Created by stijnli on 23/02/2017.
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = OrderApplication.class)
@@ -27,19 +33,32 @@ public class OrderRepositoryTest {
 
     @Inject
     private  OrderRepository orderRepository;
+    private  Customer customer;
     private Order order1,order2;
+
 
     @Before
     public void db_setup(){
-        order1 = new Order();
-        order2 = new Order();
+        customer = new Customer("Kevin","Smet");
+        order1 = new Order(new Date(2016,10,25));
+        order2 =  new Order(new Date());
+        customer.addOrder(order1);
+        customer.addOrder(order2);
+        entityManager.persist(customer);
 
-        entityManager.persist(order1);
-        entityManager.persist(order2);
+
     }
     @Test
     public void getAllShouldReturnAll(){
+        List<Order> orders = orderRepository.getAll();
+        for (Order order: orders) {
+            System.out.println(order);
+        }
         Assertions.assertThat(orderRepository.getAll()).contains(order1,order2);
+    }
+    @After
+    public void cleanDatabase(){
+        entityManager.clear();
     }
 }
 
