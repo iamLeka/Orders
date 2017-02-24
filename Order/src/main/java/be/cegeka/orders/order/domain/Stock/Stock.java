6bbe1d48a11c19506131607c326308870dfb996c
Sophie Mullen <sophie.mullen@cegeka.com>
2017-02-24 10:09:20 +0100

    ok

--- Order/src/main/java/be/cegeka/orders/order/domain/Stock/Stock.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/Stock/Stock.java
@@ -19,13 +19,13 @@ public class Stock {
     private int id;
 
     @OneToOne(cascade = ALL, mappedBy = "ITEM_ID")
-//    @JoinTable(
-//            name = "Stock",
-//            joinColumns = {@JoinColumn(name = "STOCK_ID")},
-//            inverseJoinColumns = {@JoinColumn(name = "ITEM_ID")})
     private Item item;
 
     public Stock() {
+    }
+
+    public Stock(Item item) {
+        this.item = item;
     }
 
     public int getId() {
@@ -36,7 +36,7 @@ public class Stock {
         this.item = item;
     }
 
-//    public List<Item> getItems() {
-//        return items;
-//    }
+    public Item getItem() {
+        return item;
+    }
 }
