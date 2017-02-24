dc9e400aabaf6ac6a79e88dfc23904444a6c643e
Kevin Billen <kevin.billen@cegeka.com>
2017-02-24 09:28:32 +0100

    fucking shit

---
+++ Order/src/main/java/be/cegeka/orders/order/domain/stock/Stock.java
@@ -0,0 +1,36 @@
+package be.cegeka.orders.order.domain.stock;
+
+import be.cegeka.orders.order.domain.items.Item;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STOCK")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "STOCK_ID")
+    private int stockId;
+
+    @OneToOne(cascade = CascadeType.ALL, targetEntity = Item.class)
+    @JoinColumn(name = "ITEM_ID", referencedColumnName = "ITEM_ID")
+    private Item item;
+
+    public Stock() {
+    }
+
+    public Stock(Item item) {
+        this.item = item;
+    }
+
+
+    public int getStockId() {
+        return stockId;
+    }
+
+
+    public Item getItem() {
+        return item;
+    }
+}
