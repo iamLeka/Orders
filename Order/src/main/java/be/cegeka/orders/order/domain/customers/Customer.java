cd69266db3e4112dbf399a84b3f1ea798c776559
iamleka <len.karpisek@gmail.com>
2017-11-09 14:48:45 +0100

    gwhatevr

--- Order/src/main/java/be/cegeka/orders/order/domain/customers/Customer.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/customers/Customer.java
@@ -10,17 +10,27 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CUSTOMER_ID")
     private int id;
-    @Column(name = "NAME")
-    private String name;
     @Column(name = "LAST_NAME")
     private String lastName;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ADRESS")
+    private String adress;
+    @Column(name = "PHONE")
+    private String phone;
 
     private Customer() {
     }
 
-    public Customer(String name, String lastName) {
-        this.name = name;
+    public Customer(int id, String lastName, String firstName, String email, String adress, String phone) {
+        this.id = id;
         this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.adress = adress;
+        this.phone = phone;
     }
 
     public int getId() {
