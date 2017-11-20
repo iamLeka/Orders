4785880dcbb2f6e793ab15d79d4d4917f58ed7e0
Jeroen Stroobants <jeroen.stroobants@cegeka.com>
2017-11-20 10:38:49 +0100

    OrderService test done

--- Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderServiceTest.java
+++ Order/src/test/java/be/cegeka/orders/order/domain/orders/OrderServiceTest.java
@@ -1,6 +1,11 @@
 package be.cegeka.orders.order.domain.orders;
 
 import be.cegeka.orders.order.domain.customer.Customer;
+import be.cegeka.orders.order.domain.customer.CustomerRepository;
+import be.cegeka.orders.order.domain.items.Item;
+import be.cegeka.orders.order.domain.items.ItemDto;
+import be.cegeka.orders.order.domain.items.ItemRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -8,41 +13,58 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static be.cegeka.orders.order.domain.customer.CustomerTestBuilder.aCustomer;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class OrderServiceTest {
-    private Customer customer;
+
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
-
-    @InjectMocks
-    private OrderService orderService;
 
     @Mock
     private OrderRepository orderRepository;
 
-//    @Test
-//    public void putInOrder_shouldActivateOrderRepository() throws Exception {
-//        Date date = new Date(20170823);
-//        customer = aCustomer()
-//                .withAddress("qskldfriltyr")
-//                .withEmail("qeilehanzo")
-//                .withFirstName("qsmldjkf")
-//                .withLastName("qlzdkh")
-//                .withPhone("6486784")
-//                .withID(20)
-//                .build();
-//        List<OrderItem> list = new ArrayList<>();
-//        Order order = new Order(date,list);
-//        orderService.putInOrder(20,order);
-//        verify(orderRepository).putInOrder(customer,order);
-//    }
+    @Mock
+    private OrderItemFactory orderItemFactory;
+
+    @Mock
+    private Customer customer;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Test
+    public void savingOrder_ShouldInvokeOrderRepositoryAndOrderItemFactory_AndReturnAnOrder() throws Exception {
+        Order order = new Order(Date.valueOf(LocalDate.of(2017,11,20)));
+        ItemDto itemDto1 = new ItemDto(1,5);
+        ItemDto itemDto2 = new ItemDto(2,5);
+
+        List<ItemDto> itemDtoList = Arrays.asList(itemDto1, itemDto2);
+
+        order.addOrderItem(orderItemFactory.makeOrderItemFromDto(itemDto1));
+        order.addOrderItem(orderItemFactory.makeOrderItemFromDto(itemDto2));
+
+        when(customerRepository.getCustomerById(1)).thenReturn(customer);
+
+        orderService.saveOrder(1,itemDtoList);
+
+        verify(customer).addOrder(refEq(order));
+    }
 }
